f46ef015e3f5fb2aad696c18bd14cde94053384f
rockthemicro <selitful@gmail.com>
2020-01-14 21:37:59 +0200

    setup firebase for unique id

--- app/src/main/java/com/example/mapsapp1/FirstActivity.java
+++ app/src/main/java/com/example/mapsapp1/FirstActivity.java
@@ -6,13 +6,23 @@ import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class FirstActivity extends AppCompatActivity {
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.iid.FirebaseInstanceId;
 
+public class FirstActivity extends AppCompatActivity {
+    String firebaseId = null;
+
+    {
+        FirebaseApp.initializeApp(this);
+        FirebaseInstanceId firebaseInstanceId = FirebaseInstanceId.getInstance();
+        firebaseId = firebaseInstanceId.getId();
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_first);
+
     }
 
     public void seeStadiumOnClick(View view) {
@@ -22,6 +32,8 @@ public class FirstActivity extends AppCompatActivity {
     }
 
     public void getSeatsOnClick(View view) {
+
+        System.out.println(firebaseId);System.out.println(firebaseId);System.out.println(firebaseId);System.out.println(firebaseId);
         Intent intent = new Intent(this, CategoryListActivity.class);
         startActivity(intent);
     }
