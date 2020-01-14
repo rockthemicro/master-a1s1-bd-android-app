f5754f9496a05be049e332b0f453ee09a68c477e
rockthemicro <selitful@gmail.com>
2020-01-14 22:09:06 +0200

    setup volley request for retrieving stadium sectors

--- app/src/main/java/com/example/mapsapp1/FirstActivity.java
+++ app/src/main/java/com/example/mapsapp1/FirstActivity.java
@@ -6,8 +6,20 @@ import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.iid.FirebaseInstanceId;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class FirstActivity extends AppCompatActivity {
     String firebaseId = null;
@@ -23,6 +35,35 @@ public class FirstActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_first);
 
+        RequestQueue queue = Volley.newRequestQueue(this);
+        String url = "http://" + getResources().getString(R.string.server_ipport) + "/api/app/getStadiumSectors";
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        try {
+                            JSONArray respArray = new JSONArray(response);
+                            List<String> respList = new ArrayList<>();
+
+                            for (int i = 0; i < respArray.length(); i++) {
+                                respList.add(respArray.get(i).toString());
+                            }
+                            System.out.println(respList);
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        System.out.println(error.toString());
+                    }
+                }
+        );
+
+        queue.add(stringRequest);
+
     }
 
     public void seeStadiumOnClick(View view) {
@@ -32,8 +73,6 @@ public class FirstActivity extends AppCompatActivity {
     }
 
     public void getSeatsOnClick(View view) {
-
-        System.out.println(firebaseId);System.out.println(firebaseId);System.out.println(firebaseId);System.out.println(firebaseId);
         Intent intent = new Intent(this, CategoryListActivity.class);
         startActivity(intent);
     }
